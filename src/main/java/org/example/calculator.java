5714d2ef3682821735c3d53ffd53b002b2063e72
nbhaargavi <123439300+nbhaargavi@users.noreply.github.com>
2023-03-01 15:26:34 +0530

    Update calculator.java

--- src/main/java/org/example/calculator.java
+++ src/main/java/org/example/calculator.java
@@ -18,27 +18,32 @@ public class Calculator {
         l.info("4.Division");
         l.info("Enter a choice");
         int ch=sc.nextInt();
-        Calc c=null;
+
         switch(ch)
         {
             case 1:
-                c=new Add();
-                break;
+              Add a1=new Add();
+              a1.sett(a,b);
+              l.log(Level.INFO,()->"Addition of two numbers"+a1.cal(a1.get1(),a1.get2()));
+              break;
             case 2:
-                c=new Sub();
+                Sub s=new Sub();
+                s.sett(a,b);
+                l.log(Level.INFO,()->"Subtraction of two numbers" + s.cal(s.get1(),s.get2()));
                 break;
             case 3:
-                c=new Mul();
+                Mul m=new Mul();
+                m.sett(a,b);
+                l.log(Level.INFO,()->"Multiplication of two numbers"+m.cal(m.get1(),m.get2()));
                 break;
             case 4:
-               c=new Div();
+                Div d=new Div();
+                d.sett(a,b);
+                l.log(Level.INFO,()->"Division of two numbers"+d.cal(d.get1(),d.get2()));
                 break;
             default:
                 l.info("Valid input required ");
         }
-        c.sett(a,b);
-        Calc k = c;
-        l.log(Level.INFO,() ->"Result: " + k.cal(k.get1(),k.get2()));
         sc.close();
     }
 }
