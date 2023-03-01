f5a768370f1f4bcff6be11be0ab0d1d5b7a92a2c
nbhaargavi <123439300+nbhaargavi@users.noreply.github.com>
2023-03-01 15:27:14 +0530

    Update Calc.java

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -2,20 +2,22 @@ package org.example;
 
 abstract class Calc {
         Logger l=Logger.getLogger("kitty");
-        private int a;
-        private int b;
+        private int n1;
+        private int n2;
+
         public void sett(int a,int b)
         {
-            this.a=a;
-            this.b=b;
+            this.n1=a;
+            this.n2=b;
         }
         int get1()
         {
-            return a;
+            return n1;
         }
         int get2()
         {
-            return b;
+            return n2;
         }
         abstract int cal(int a,int b);
     }
+
